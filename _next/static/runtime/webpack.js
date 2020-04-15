f6f1e8b20f9aea059a1f4f2d18b3582030f704a1
hkgsherlock <hkgsherlock@gmail.com>
2020-04-15 09:47:15 +0800

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b3218a691f36491448d5";
+/******/ 	var hotCurrentHash = "f8fa7746248701e18041";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/static/development/pages/index.js.ef2a084e976d57c5f9ae.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./pages/img/32c35668e16c936f9c5f.jpg":
+/*!********************************************!*\
+  !*** ./pages/img/32c35668e16c936f9c5f.jpg ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/32c35668e16c936f9c5f-daf26d06d7429b5a41001d602924cc5c.jpg";
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.ef2a084e976d57c5f9ae.hot-update.js.map
